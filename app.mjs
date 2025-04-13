f73aa5b4d488b6d7736fbe786dc143f8e11e1043
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-04-13 10:53:42 +0700

    feat: create questions api

--- app.mjs
+++ app.mjs
@@ -1,13 +1,17 @@
 import express from "express";
+import questionsRouter from "./routes/questionsRouter.mjs";
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use("/questions", questionsRouter);
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
+
+
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
